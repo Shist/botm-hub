b02d7c286ab3f1252cfd02af7aef9fe3b9f2e891
Shist <shist.pupust@mail.ru>
2025-10-06 22:15:06 +0300

    MapUpload component was refactored to upload arrays of maps

--- src/components/MapUpload.vue
+++ src/components/MapUpload.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="map-upload-wrapper">
-    <h3 class="map-upload-wrapper__title">Информация о карте для загрузки:</h3>
-    <ul class="map-upload-wrapper__list">
+    <h3 class="map-upload-wrapper__title">Информация о картах для загрузки:</h3>
+    <ul
+      v-for="mapData in arrayOfMapsData"
+      :key="mapData.id"
+      class="map-upload-wrapper__list"
+    >
       <li
         v-for="(value, key) in mapData"
         :key="key"
@@ -11,51 +15,70 @@
         <span class="map-upload-wrapper__list-item-value">{{ value }}</span>
       </li>
     </ul>
-    <button class="map-upload-wrapper__upload-btn" @click="uploadMap">
-      Загрузить карту
+    <button class="map-upload-wrapper__upload-btn" @click="uploadMaps">
+      Загрузить карты
     </button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { uploadMapToFirebase } from "@/services/firebase";
-import { OsuMapCategory } from "@/types";
+import { OsuMapCategory, type IOsuMap } from "@/types";
 
-const mapData = {
-  id: 2907027,
-  ar: 10.1,
-  bpm: 233,
-  category: "dt1",
-  comment: "6WC25 GF (Speed aim DT1)",
-  cs: 4.2,
-  duration: "01:47",
-  hp: 5.4,
-  mapper: "SuckMyAnimeXDD",
-  name: "Lida, GSPD - Eurobit [Eurobit]",
-  od: 10.1,
-  starRate: 5.86,
-};
+const arrayOfMapsData = [
+  {
+    id: 2907027,
+    ar: 10.1,
+    bpm: 233,
+    category: "dt1",
+    comment: "6WC25 GF (Speed aim DT1)",
+    cs: 4.2,
+    duration: "01:47",
+    hp: 5.4,
+    mapper: "SuckMyAnimeXDD",
+    name: "Lida, GSPD - Eurobit [Eurobit]",
+    od: 10.1,
+    starRate: 5.86,
+  },
+  {
+    id: 1275050,
+    ar: 9,
+    bpm: 186,
+    category: "nm2",
+    comment: "из коллекции D33MKA",
+    cs: 4,
+    duration: "01:48",
+    hp: 5,
+    mapper: "HighTec",
+    name: "positive MAD-crew – Lapistoria no yakusoku [Shizuku's Hyper]",
+    od: 7.5,
+    starRate: 4.38,
+  },
+];
 
-const uploadMap = async () => {
-  const mapId = mapData.id;
-  const mapInfo = {
-    ar: mapData.ar,
-    bpm: mapData.bpm,
-    category: OsuMapCategory[mapData.category as OsuMapCategory],
-    comment: mapData.comment,
-    cs: mapData.cs,
-    duration: mapData.duration,
-    hp: mapData.hp,
-    mapper: mapData.mapper,
-    name: mapData.name,
-    od: mapData.od,
-    starRate: mapData.starRate,
-  };
-  try {
-    await uploadMapToFirebase(mapId, mapInfo);
-    console.log("Успешная загрузка!!!");
-  } catch (error) {
-    console.error(error);
+const uploadMaps = async () => {
+  for (let i = 0; i < arrayOfMapsData.length; i++) {
+    const mapData = arrayOfMapsData[i] as Omit<IOsuMap, "link">;
+    const mapId = mapData.id;
+    const mapInfo = {
+      ar: mapData.ar,
+      bpm: mapData.bpm,
+      category: OsuMapCategory[mapData.category as OsuMapCategory],
+      comment: mapData.comment,
+      cs: mapData.cs,
+      duration: mapData.duration,
+      hp: mapData.hp,
+      mapper: mapData.mapper,
+      name: mapData.name,
+      od: mapData.od,
+      starRate: mapData.starRate,
+    };
+    try {
+      await uploadMapToFirebase(mapId, mapInfo);
+      console.log("Успешная загрузка!!!");
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
 </script>
@@ -63,16 +86,19 @@ const uploadMap = async () => {
 <style lang="scss" scoped>
 .map-upload-wrapper {
   padding: 20px;
+  background-color: black;
   &__title {
     @include default-headline(24px, 24px, var(--color-text));
     margin-bottom: 20px;
   }
   &__list {
+    margin-bottom: 10px;
     padding: 20px;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     row-gap: 10px;
+    border: 2px solid var(--color-btn-bg);
   }
   &__list-item {
     display: grid;
