d2bbb71befbd5e9639283a81298b6ac05e3a2819
Shist <shist.pupust@mail.ru>
2025-10-11 20:40:37 +0300

    Refactored paddings for skillsets maps pages; improved maps uploading

--- src/components/maps-upload/MapsUpload.vue
+++ src/components/maps-upload/MapsUpload.vue
@@ -2,7 +2,7 @@
   <div class="map-upload-wrapper">
     <h3 class="map-upload-wrapper__title">Информация о картах для загрузки:</h3>
     <ul
-      v-for="mapData in arrayOfMapsData"
+      v-for="mapData in mapsArr"
       :key="mapData.id"
       class="map-upload-wrapper__list"
     >
@@ -23,42 +23,12 @@
 
 <script setup lang="ts">
 import { uploadMapToFirebase } from "@/services/firebase";
+import mapsArr from "@/components/maps-upload/maps.json";
 import { OsuMapCategory, type IOsuMap } from "@/types";
 
-const arrayOfMapsData = [
-  {
-    id: 2907027,
-    ar: 10.1,
-    bpm: 233,
-    category: "dt1",
-    comment: "6WC25 GF (Speed aim DT1)",
-    cs: 4.2,
-    duration: "01:47",
-    hp: 5.4,
-    mapper: "SuckMyAnimeXDD",
-    name: "Lida, GSPD - Eurobit [Eurobit]",
-    od: 10.1,
-    starRate: 5.86,
-  },
-  {
-    id: 1275050,
-    ar: 9,
-    bpm: 186,
-    category: "nm2",
-    comment: "из коллекции D33MKA",
-    cs: 4,
-    duration: "01:48",
-    hp: 5,
-    mapper: "HighTec",
-    name: "positive MAD-crew – Lapistoria no yakusoku [Shizuku's Hyper]",
-    od: 7.5,
-    starRate: 4.38,
-  },
-];
-
 const uploadMaps = async () => {
-  for (let i = 0; i < arrayOfMapsData.length; i++) {
-    const mapData = arrayOfMapsData[i] as Omit<IOsuMap, "link">;
+  for (let i = 0; i < mapsArr.length; i++) {
+    const mapData = mapsArr[i] as Omit<IOsuMap, "link">;
     const mapId = mapData.id;
     const mapInfo = {
       ar: mapData.ar,
